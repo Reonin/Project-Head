a13427785b799a0c29d260235ff7e3a0a8573e3d
Reonin <coreyajeffers@gmail.com>
2016-01-10 17:31:27 -0500

    Sup Player
    Added a proper player class for player object instantiation

--- libraries/player.js
+++ libraries/player.js
@@ -11,415 +11,6 @@ var titleScreen = new Image();
 titleScreen.src = 'images/TitleScreen.png';
 
 
-//Create The player
-var player = {
-    // color: "#00A",
-    sprite: Sprite("spaceship"),
-    x: 220,
-    y: 680,
-    width: 32,
-    height: 32,
-    life: 100,
-    velX: 0,
-    velY: 0,
-    speed: 4,
-    pointScore:0,
-    order:1,
-    name: "Player 1",
-    aka: "player1",
-    friction: 0.85,
-    pilotImg:grayPilot,
-    color:"#D3D3D3",
-    draw: function() {
-        //canvas.fillStyle = this.color;
-        // canvas.fillRect(this.x, this.y, this.width, this.height);
-        this.sprite.draw(canvas, this.x, this.y);
-    },
-    shoot: function() {
-        var bulletPosition = this.midpoint();
-        shoot_sound.play();
-
-
-        playerBullets.push(Bullet({
-            speed: 5,
-            x: bulletPosition.x,
-            y: bulletPosition.y,
-            bulletID: "player1"
-        }))
-
-        setTimeout("reload1()", 500);
-
-    },
-    movement: function(){
-
-        if (keydown.left) {
-           if (player.velX > -player.speed) {
-                player.velX--;
-            }
-        }
-
-        if (keydown.right) {
-           if (player.velX < player.speed) {
-                player.velX++;
-            }
-        }
-
-        if (keydown.up) {
-            if (player.velY > -player.speed) {
-                player.velY--;
-            }
-
-
-        }
-
-        if (keydown.down) {
-            if (player.velY < player.speed) {
-                player.velY++;
-            }
-
-        }
-
-        player.velX *= player.friction;
-        player.x += player.velX;
-
-        player.velY *= player.friction;
-        player.y += player.velY;
-
-
-
-        player.x = player.x.clamp(0, CANVAS_WIDTH - player.width); //prevents character from going past canvas
-
-
-        player.y = player.y.clamp(0, CANVAS_HEIGHT - player.height); //prevents character from going past canvas
-
-    },
-    launch: function(){
-        var missilePostition = this.midpoint();
-        console.log(Missle.width);
-        playerMissiles.push(Missle({
-            speed: 2,
-            x: missilePostition.x - 500,
-            y: missilePostition.y
-        }))
-    },
-    midpoint: function() {
-        return {
-            x: this.x + this.width / 2,
-            y: this.y + this.height / 2
-        };
-    },
-    explode: function() {
-        this.active = false;
-        explosion_sound.play();
-        GameLoopMusic_sound.fadeOut(0, 2000);
-        currentState = states.End;
-        // An explosion sound and then end the game
-    },
-    lifeChange: function(change) {
-
-
-        this.life = this.life + change; //Adds or subtracts health based on the value added in the function
-
-        if (this.life <= 0) {
-            this.explode();
-        }
-        if(this.life > 100){
-            this.life = 100;
-        }
-
-        return this.life;
-
-
-    },
-    score: function(change) {
-
-
-        this.pointScore = this.pointScore + change; //Adds or subtracts health based on the value added in the function
-
-        /*if (this.life <= 0) {
-            this.explode();
-        }*/
-
-        return this.pointScore;
-
-
-    },
-
-
-    isHit: false,
-    quip: ""
-};
-
-
-
-//Create The player
-var player2 = {
-    // color: "#00A",
-    sprite: Sprite("pixel_spaceship"),
-    x: 260,
-    y: 680,
-    width: 32,
-    height: 32,
-    life: 100,
-    velX: 0,
-    velY: 0,
-    speed: 4,
-    pointScore:0,
-    order:2,
-    name: "Player 2",
-    aka: "player2",
-    friction: 0.85,
-    pilotImg:bluePilot,
-    color:"#0000FF",
-    draw: function() {
-        //canvas.fillStyle = this.color;
-        // canvas.fillRect(this.x, this.y, this.width, this.height);
-        this.sprite.draw(canvas, this.x, this.y);
-    },
-    shoot: function() {
-        var bulletPosition = this.midpoint();
-        shoot_sound.play();
-
-        playerBullets.push(Bullet({
-            speed: 5,
-            x: bulletPosition.x,
-            y: bulletPosition.y,
-            bulletID: "player2"
-        }))
-
-        setTimeout("reload2()", 500);
-    },
-    movement: function(){
-
-        if (keydown.a) {
-           if (player2.velX > -player2.speed) {
-                player2.velX--;
-            }
-        }
-
-        if (keydown.d) {
-           if (player2.velX < player2.speed) {
-                player2.velX++;
-            }
-        }
-
-        if (keydown.w) {
-            if (player2.velY > -player2.speed) {
-                player2.velY--;
-            }
-
-
-        }
-
-        if (keydown.s) {
-            if (player2.velY < player2.speed) {
-                player2.velY++;
-            }
-
-        }
-
-        player2.velX *= player2.friction;
-        player2.x += player2.velX;
-
-        player2.velY *= player2.friction;
-        player2.y += player2.velY;
-
-
-
-        player2.x = player2.x.clamp(0, CANVAS_WIDTH - player2.width); //prevents character from going past canvas
-
-
-        player2.y = player2.y.clamp(0, CANVAS_HEIGHT - player2.height); //prevents character from going past canvas
-
-    },
-    launch: function(){
-        var missilePostition = this.midpoint();
-        console.log(Missle.width);
-        playerMissiles.push(Missle({
-            speed: 2,
-            x: missilePostition.x - 500,
-            y: missilePostition.y
-        }))
-    },
-    midpoint: function() {
-        return {
-            x: this.x + this.width / 2,
-            y: this.y + this.height / 2
-        };
-    },
-    explode: function() {
-        this.active = false;
-        explosion_sound.play();
-        GameLoopMusic_sound.fadeOut(0, 2000);
-        currentState = states.End;
-        // An explosion sound and then end the game
-    },
-    lifeChange: function(change) {
-
-
-        this.life = this.life + change; //Adds or subtracts health based on the value added in the function
-
-        if (this.life <= 0) {
-            this.explode();
-        }
-        if(this.life > 100){
-            this.life = 100;
-        }
-        return this.life;
-
-
-    },
-    score: function(change) {
-
-
-        this.pointScore = this.pointScore + change; //Adds or subtracts health based on the value added in the function
-
-        /*if (this.life <= 0) {
-            this.explode();
-        }*/
-
-        return this.pointScore;
-
-
-    },isHit: false,
-    quip: ""
-};
-
-
-
-//Create The player
-var player3 = {
-    // color: "#00A",
-    sprite: Sprite("redShip"),
-    x: 300,
-    y: 680,
-    width: 32,
-    height: 32,
-    life: 100,
-    velX: 0,
-    velY: 0,
-    speed: 4,
-    pointScore:0,
-    order:3,
-    name: "Player 3",
-    aka: "player3",
-    friction: 0.85,
-    pilotImg:redPilot,
-    color:"#FF0000",
-    draw: function() {
-        //canvas.fillStyle = this.color;
-        // canvas.fillRect(this.x, this.y, this.width, this.height);
-        this.sprite.draw(canvas, this.x, this.y);
-    },
-    shoot: function() {
-        var bulletPosition = this.midpoint();
-        shoot_sound.play();
-
-        playerBullets.push(Bullet({
-            speed: 5,
-            x: bulletPosition.x,
-            y: bulletPosition.y,
-            bulletID: "player3"
-        }))
-
-        setTimeout("reload3()", 500);
-    },
-    movement: function(){
-
-        if (keydown.j) {
-           if (player3.velX > -player3.speed) {
-                player3.velX--;
-            }
-        }
-
-        if (keydown.l) {
-           if (player3.velX < player3.speed) {
-                player3.velX++;
-            }
-        }
-
-        if (keydown.i) {
-            if (player3.velY > -player3.speed) {
-                player3.velY--;
-            }
-
-
-        }
-
-        if (keydown.k) {
-            if (player3.velY < player3.speed) {
-                player3.velY++;
-            }
-
-        }
-
-        player3.velX *= player3.friction;
-        player3.x += player3.velX;
-
-        player3.velY *= player3.friction;
-        player3.y += player3.velY;
-
-
-
-        player3.x = player3.x.clamp(0, CANVAS_WIDTH - player3.width); //prevents character from going past canvas
-
-
-        player3.y = player3.y.clamp(0, CANVAS_HEIGHT - player3.height); //prevents character from going past canvas
-
-    },
-    launch: function(){
-        var missilePostition = this.midpoint();
-        console.log(Missle.width);
-        playerMissiles.push(Missle({
-            speed: 2,
-            x: missilePostition.x - 500,
-            y: missilePostition.y
-        }))
-    },
-    midpoint: function() {
-        return {
-            x: this.x + this.width / 2,
-            y: this.y + this.height / 2
-        };
-    },
-    explode: function() {
-        this.active = false;
-        explosion_sound.play();
-        GameLoopMusic_sound.fadeOut(0, 2000);
-        currentState = states.End;
-        // An explosion sound and then end the game
-    },
-    lifeChange: function(change) {
-
-
-        this.life = this.life + change; //Adds or subtracts health based on the value added in the function
-
-        if (this.life <= 0) {
-            this.explode();
-        }
-        if(this.life > 100){
-            this.life = 100;
-        }
-        return this.life;
-
-
-    },
-    score: function(change) {
-
-
-        this.pointScore = this.pointScore + change; //Adds or subtracts health based on the value added in the function
-
-        /*if (this.life <= 0) {
-            this.explode();
-        }*/
-
-        return this.pointScore;
-
-
-    },isHit: false,
-    quip: ""
-};
-
 var playerBullets = [];
 
 function Bullet(I) {
@@ -493,4 +84,159 @@ function reload2(){
 
 function reload3(){
     alreadyShot3 = true;
+};
+
+//The Player Class
+function PlayerCreator(spriteimg,name,alias,order,pilotImage,color,reload,startingX,startingY){
+
+      // color: "#00A",
+      this.sprite = Sprite(spriteimg);
+      this.x = startingX;
+      this.y = startingY;
+      this.width = 32;
+      this.height = 32;
+      this.life = 100;
+      this.velX = 0;
+      this.velY = 0;
+      this.speed = 4;
+      this.pointScore = 0;
+      this.order = order;
+      this.name = name;
+      this.aka = alias;
+      this.friction = 0.85;
+      this.pilotImg = pilotImage;
+      this.color = color;
+      this.draw = function() {
+          //canvas.fillStyle = this.color;
+          // canvas.fillRect(this.x, this.y, this.width, this.height);
+          this.sprite.draw(canvas, this.x, this.y);
+      };
+      this.shoot = function() {
+          var bulletPosition = this.midpoint();
+          shoot_sound.play();
+
+          playerBullets.push(Bullet({
+              speed: 5,
+              x: bulletPosition.x,
+              y: bulletPosition.y,
+              bulletID: alias
+          }))
+
+          setTimeout(reload, 500);
+      };
+    this.movement = function(left,right,up,down){
+    // debugger;
+    if(alias == "player1"){
+      left = keydown.left;
+      right = keydown.right;
+      up = keydown.up;
+      down = keydown.down;
+    }
+    if(alias == "player2"){
+      left = keydown.a;
+      right = keydown.d;
+      up = keydown.w;
+      down = keydown.s;
+    }
+    if(alias == "player3"){
+      left = keydown.j;
+      right = keydown.l;
+      up = keydown.i;
+      down = keydown.k;
+    }
+          if (left) {
+             if (this.velX > -this.speed) {
+                  this.velX--;
+              }
+          }
+
+          if (right) {
+             if (this.velX < this.speed) {
+                  this.velX++;
+              }
+          }
+
+          if (up) {
+              if (this.velY > -this.speed) {
+                  this.velY--;
+              }
+
+
+          }
+
+          if (down) {
+              if (this.velY < this.speed) {
+                  this.velY++;
+              }
+
+          }
+
+          this.velX *= this.friction;
+          this.x += this.velX;
+
+          this.velY *= this.friction;
+          this.y += this.velY;
+
+
+
+          this.x = this.x.clamp(0, CANVAS_WIDTH - this.width); //prevents character from going past canvas
+
+
+          this.y = this.y.clamp(0, CANVAS_HEIGHT - this.height); //prevents character from going past canvas
+
+      };
+      this.launch = function(){
+          var missilePostition = this.midpoint();
+          console.log(Missle.width);
+          playerMissiles.push(Missle({
+              speed: 2,
+              x: missilePostition.x - 500,
+              y: missilePostition.y
+          }))
+      };
+      this.midpoint = function() {
+          return {
+              x: this.x + this.width / 2,
+              y: this.y + this.height / 2
+          };
+      };
+      this.explode = function() {
+          this.active = false;
+          explosion_sound.play();
+          GameLoopMusic_sound.fadeOut(0, 2000);
+          currentState = states.End;
+          // An explosion sound and then end the game
+      };
+      this.lifeChange = function(change) {
+
+
+          this.life = this.life + change; //Adds or subtracts health based on the value added in the function
+
+          if (this.life <= 0) {
+              this.explode();
+          }
+          if(this.life > 100){
+              this.life = 100;
+          }
+          return this.life;
+
+
+      };
+      this.score =function(change) {
+
+
+          this.pointScore = this.pointScore + change; //Adds or subtracts health based on the value added in the function
+
+          /*if (this.life <= 0) {
+              this.explode();
+          }*/
+
+          return this.pointScore;
+
+
+      };
+      this.isHit = false;
+      this.quip = "";
+
+
 };
